543915f62493c2a593501bc8b3b697e09809a1f1
JustWaveThings <justwavethings@proton.me>
2023-04-12 11:26:37 -0400

    Add array of objects for the different inputs

--- cv-app/src/App.js
+++ cv-app/src/App.js
@@ -7,4 +7,35 @@ export class App extends Component {
 	}
 }
 
+const TEST = [
+	{
+		Profile: 'Contact Section',
+		name: 'name',
+		email: 'email',
+		phone: 'phone',
+		edit: 'edit',
+		submit: 'submit',
+	},
+	{
+		Education: 'Education Section',
+		school: 'school',
+		degree: 'degree',
+		year: 'year',
+		edit: 'edit',
+		addNew: 'add new',
+		submit: 'submit',
+	},
+	{
+		Experience: 'Experience Section',
+		company: 'company',
+		position: 'position',
+		yearStart: 'Year Started',
+		yearEnd: 'Year Ended',
+		description: 'Duties Performed',
+		edit: 'edit',
+		addNew: 'add new',
+		submit: 'submit',
+	},
+];
+
 export default App;
