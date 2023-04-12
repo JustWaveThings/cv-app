f9a0420cebe005e2da80f6e0e103dad3d2eb85c5
JustWaveThings <justwavethings@proton.me>
2023-04-12 10:51:21 -0400

    Change to class component

--- cv-app/src/App.js
+++ cv-app/src/App.js
@@ -1,7 +1,10 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-	return <div className="App"></div>;
+export class App extends Component {
+	render() {
+		return <div className="App">App</div>;
+	}
 }
 
 export default App;
